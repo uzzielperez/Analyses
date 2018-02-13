87b16b5eb3ce496e90d350c355d8d90ba6ccef9f
uzziel ciliciaperez <uzzie.perez@cern.ch>
2018-02-13 16:20:16 -0600

    improved runChainClass.py

---
+++ Analysis_v1/GGJetsStudy/analyzeClassGGJets.C
@@ -0,0 +1,21 @@
+#include "ClassGGJets.C" 
+#include <iostream>
+#include "TStopwatch.h"
+using namespace std;
+
+int analyze(){
+        // start stopwatch
+	TStopwatch sw;
+	sw.Start();
+
+	ClassGGJets t;
+        t.Loop();
+		
+	// stop stopwatch
+	sw.Stop();
+	cout << "Real Time: " << sw.RealTime()/60.0 << " minutes" << endl;
+	cout << "CPU Time: " << sw.CpuTime()/60.0 << " minutes" << endl;
+	return 0;
+	
+}
+
