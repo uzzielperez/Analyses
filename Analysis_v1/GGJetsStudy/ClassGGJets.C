271cca3c22f15a74eb4c928ba5301fdf302cf243
uzziel ciliciaperez <uzzie.perez@cern.ch>
2018-02-17 21:05:27 -0600

    added weight adjustments (needs checking)

--- Analysis_v1/GGJetsStudy/ClassGGJets.C
+++ Analysis_v1/GGJetsStudy/ClassGGJets.C
@@ -4,6 +4,8 @@
 #include <TStyle.h>
 #include <TCanvas.h>
 
+#include <fstream>
+
 void ClassGGJets::Loop()
 {
 //   In a ROOT session, you can do:
@@ -30,23 +32,28 @@ void ClassGGJets::Loop()
 //    fChain->GetEntry(jentry);       //read all branches
 //by  b_branchname->GetEntry(ientry); //read only this branch
    if (fChain == 0) return;
+	// fileout
+	ofstream weightinfo;
+	weightinfo.open("weightinfo.txt",ios::app);
 
 	// counters
   	int nEBEB = 0;
   	int nEBEEorEEEB = 0;
   	int nPassisGood = 0;
         int Ntotal = 0;
+	
+	int N500to1000 =0;
 
 	//histograms
         Long64_t nentries = fChain->GetEntriesFast();
-	diphotonMinv = new TH1D("diphotonMinv", "", 50, 0.,1600.);
+	TH1D* diphotonMinv = new TH1D("diphotonMinv", "", 50, 0.,2000.);
 	diphotonMinv->Sumw2();
-	TH1D* photon1Pt  = new TH1D("photon1Pt", "", 50, 0.,1600.);
+	TH1D* photon1Pt  = new TH1D("photon1Pt", "", 50, 0.,2000.);
 	TH1D* photon1Eta = new TH1D("photon1Eta", "", 80, -3,3);
-	TH1D* photon1Phi = new TH1D("photon1Phi", "", 80, -3,3);
-	TH1D* photon2Pt  = new TH1D("photon2Pt", "", 80, 0.,1600.);
+	TH1D* photon1Phi = new TH1D("photon1Phi", "", 80, -3.5,3.5);
+	TH1D* photon2Pt  = new TH1D("photon2Pt", "", 80, 0.,2000.);
 	TH1D* photon2Eta = new TH1D("photon2Eta", "", 80, -3,3);
-	TH1D* photon2Phi = new TH1D("photon2Phi", "", 80, -3,3);
+	TH1D* photon2Phi = new TH1D("photon2Phi", "", 80, -3.5,3.5);
 	photon1Pt->Sumw2();
 	photon1Eta->Sumw2();
 	photon1Phi->Sumw2();
@@ -54,53 +61,110 @@ void ClassGGJets::Loop()
 	photon2Eta->Sumw2();
 	photon2Phi->Sumw2();
 	
-	TH1D* diphotonMinvEBEB = new TH1D("diphotonMinvEBEB","",80,0.,1600.);
-        TH1D* diphotonMinvEBEE = new TH1D("diphotonMinvEBEE","",80,0.,1600.);
+	TH1D* diphotonMinvEBEB = new TH1D("diphotonMinvEBEB","",80,0.,2000.);
+        TH1D* diphotonMinvEBEE = new TH1D("diphotonMinvEBEE","",80,0.,2000.);
         diphotonMinvEBEB->Sumw2();
         diphotonMinvEBEE->Sumw2();
-        TH1D* photon1PtEBEB = new TH1D("photon1PtEBEB","",80,0.,1600.);
-        TH1D* photon1PtEBEE = new TH1D("photon1PtEBEE","",80,0.,1600.);
+        TH1D* photon1PtEBEB = new TH1D("photon1PtEBEB","",80,0.,2000.);
+        TH1D* photon1PtEBEE = new TH1D("photon1PtEBEE","",80,0.,2000.);
         photon1PtEBEB->Sumw2();
         photon1PtEBEE->Sumw2();
-        TH1D* photon2PtEBEB = new TH1D("photon2PtEBEB","",80,0.,1600.);
- 	TH1D* photon2PtEBEE = new TH1D("photon2PtEBEE","",80,0.,1600.);
+        TH1D* photon2PtEBEB = new TH1D("photon2PtEBEB","",80,0.,2000.);
+ 	TH1D* photon2PtEBEE = new TH1D("photon2PtEBEE","",80,0.,2000.);
  	photon2PtEBEB->Sumw2();
  	photon2PtEBEE->Sumw2();
 
+	// Detector Eta and Phi
+	TH1D* photon1detEta = new TH1D("photon1detEta", "", 80, -3,3);
+	TH1D* photon1detPhi = new TH1D("photon1detPhi", "", 80, -3.5,3.5);
+	TH1D* photon2detEta = new TH1D("photon2detEta", "", 80, -3,3);
+	TH1D* photon2detPhi = new TH1D("photon2detPhi", "", 80, -3.5,3.5);
+	photon1detEta->Sumw2();
+	photon1detPhi->Sumw2();
+	photon2detEta->Sumw2();
+	photon2detPhi->Sumw2();
+	
    Long64_t nbytes = 0, nb = 0;
    for (Long64_t jentry=0; jentry<nentries;jentry++) {
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
       // if (Cut(ientry) < 0) continue;
-      if (jentry%10000 == 0) cout << "Number of processed events: "<< jentry << endl;
-      Ntotal++;
-
+      Ntotal++; 
+	//float weight = 0;
+	float weight = Event_weightAll;
+	float newLumi = 0;
+//         if (jentry < 571197){
+//		  if (jentry == 0) weightinfo << " LumiWeight: " << Event_weightLumi <<  endl;
+//		//weight = Event_weight*Event_weightLumi;
+//		  newLumi = 9.62043852292e-05;
+//		  weight = Event_weight*newLumi;
+//	}
+//         if (jentry > 571196 && jentry < 571197+299999){
+//		 if (jentry == 571197) weightinfo << " LumiWeight: " << Event_weightLumi <<  endl;
+//		 newLumi = 6.23531019051e-06; 
+//		 weight = Event_weight*newLumi;		
+//	} 
+//	 if (jentry > 571197+299998  && jentry < 571197+299999+2449553){
+//		 if (jentry == 571197+299999) weightinfo << " LumiWeight: " << Event_weightLumi <<  endl; 
+//                 newLumi = 0.00464394979838;	
+//		 weight = Event_weight*newLumi;
+//	}
+//	 if (jentry > 571197+299999+2449552 && jentry < 5711977+299999+2449553+691454){
+//	       	 if (jentry == 571197+299999+2449553) weightinfo << " LumiWeight: " << Event_weightLumi <<  endl;
+//	 	 newLumi = 4.34847977414e-08; 
+//		 weight = Event_weight*newLumi;
+//        }
+//	 if (jentry > 5711977+299999+2449553+691453 && jentry < 571197+299999+2449553+691454+249872){
+//		 if (jentry == 5711977+299999+2449553+691454) weightinfo << " LumiWeight: " << Event_weightLumi <<  endl;
+//	  	 newLumi = 0.001107044703;
+//		 weight = Event_weight*newLumi;
+//	}
+//         if (jentry > 571197+299999+2449553+691454+249871 && jentry < 571197+299999+2449553+691454+249872+99991){
+//		 if (jentry == 571197+299999+2449553+691454+249872) weightinfo << " LumiWeight: " << Event_weightLumi <<  endl;
+//		 newLumi = 3.55644279431e-10;
+//		 weight = Event_weight*newLumi;
+//	}
+//	 if (jentry > 571197+299999+2449553+691454+249872+99991){
+//	         if (jentry == 571197+299999+2449553+691454+249872+99991) weightinfo << " LumiWeight: " << Event_weightLumi << endl;
+//		 newLumi = 0.0293264015104;
+//		 weight = Event_weight*newLumi;
+//	}
+	      
+//  if (jentry%10000 == 0) cout << "Number of processed events: "<< jentry << "; LumiWeight: " << Event_weightLumi << "; Weight:" << Event_weightAll <<  "; NewLumiW: " << newLumi << "; NewWeight: " << weight << endl;
+    
+      if (jentry%10000 == 0) cout << "Number of processed events: " << jentry << endl;
+	     
       if (isGood){ 
         nPassisGood++;
-        diphotonMinv->Fill(Diphoton_Minv, Event_weightAll);
-	photon1Pt->Fill(Photon1_pt, Event_weightAll);
-	photon1Eta->Fill(Photon1_eta, Event_weightAll);
-	photon1Phi->Fill(Photon1_phi, Event_weightAll);
-	photon2Pt->Fill(Photon2_pt, Event_weightAll);
-	photon2Eta->Fill(Photon2_eta, Event_weightAll);
-	photon2Phi->Fill(Photon2_phi, Event_weightAll);
+        diphotonMinv->Fill(Diphoton_Minv, weight);
+	photon1Pt->Fill(Photon1_pt, weight);
+	photon1Eta->Fill(Photon1_eta, weight);
+	photon1Phi->Fill(Photon1_phi, weight);
+	photon2Pt->Fill(Photon2_pt, weight);
+	photon2Eta->Fill(Photon2_eta, weight);
+	photon2Phi->Fill(Photon2_phi, weight);
 	
+	photon1detEta->Fill(Photon1_scEta, weight);
+	photon1detPhi->Fill(Photon1_scPhi, weight); 
+	photon2detEta->Fill(Photon2_scEta, weight);
+	photon2detPhi->Fill(Photon2_scPhi, weight); 
+
 	//EBEB
 	if (fabs(Photon1_scEta) < 1.4442 && fabs(Photon2_scEta) < 1.4442) {
                 nEBEB++;
-		diphotonMinvEBEB->Fill(Diphoton_Minv);
-		photon1PtEBEB->Fill(Photon1_pt);
-		photon2PtEBEB->Fill(Photon2_pt);
+		diphotonMinvEBEB->Fill(Diphoton_Minv, weight);
+		photon1PtEBEB->Fill(Photon1_pt, weight);
+		photon2PtEBEB->Fill(Photon2_pt, weight);
 	}//end EBEB
 	
 	// EBEE or EEBE 
 	if ((fabs(Photon1_scEta) < 1.4442 && 1.566 < fabs(Photon2_scEta) && fabs(Photon2_scEta) < 2.5) ||
 	(fabs(Photon2_scEta) < 1.4442 && 1.566 < fabs(Photon1_scEta) && fabs(Photon1_scEta) < 2.5) ) {
      		nEBEEorEEEB++;
-		diphotonMinvEBEE->Fill(Diphoton_Minv);
-		photon1PtEBEE->Fill(Photon1_pt);
-		photon2PtEBEE->Fill(Photon2_pt);
+		diphotonMinvEBEE->Fill(Diphoton_Minv, weight);
+		photon1PtEBEE->Fill(Photon1_pt, weight);
+		photon2PtEBEE->Fill(Photon2_pt, weight);
 	}//end EBEE or EEBE
 	}//end isGood
 
@@ -131,4 +195,10 @@ void ClassGGJets::Loop()
         photon1PtEBEE->Write();
         photon2PtEBEE->Write();
 
+	photon1detEta->Write();
+	photon1detPhi->Write();
+	photon2detEta->Write();
+	photon2detPhi->Write();
+
+	weightinfo.close();		
 }//end Class
