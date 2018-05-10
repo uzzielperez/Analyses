16a58a75b29eb951abfbe37291e983f0a87e86ee
Uzziel Perez <uzzie.perez@protonmail.com>
2018-05-10 09:25:30 +0200

    Compiling, accessing MC level info

--- DiPhotonAnalyzer/plugins/DiPhotonAnalyzer.cc
+++ DiPhotonAnalyzer/plugins/DiPhotonAnalyzer.cc
@@ -16,9 +16,10 @@
 //
 //
 
-
+using namespace std; 
 // system include files
 #include <memory>
+#include <vector>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -31,6 +32,20 @@
 
 // Misc.
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+
+//TFileService
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+//for the GenParticleCollection and GenParticles
+#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
+#include <vector>
+#include "TLorentzVector.h"
+#include "TH2D.h"
+#include "TTree.h"
+
 
 
 //
@@ -57,6 +72,37 @@ class DiPhotonAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources> 
       virtual void endJob() override;
 
       // ----------member data ---------------------------
+      edm::Service<TFileService> fs;
+      edm::EDGetTokenT<vector<reco::GenParticle> > genParticlesToken_;
+      edm::InputTag genParticles_;
+      edm::InputTag particles_;
+
+       TTree *fgenTree;
+      //Structs
+      struct eventInfo_t {
+      Long64_t run;
+      Long64_t LS;
+      Long64_t evnum;
+      };
+      eventInfo_t fEventInfo; //ExoDiPhotons::eventInfo_t fEventInfo;
+   
+      struct genPhotonInfo_t{
+        double pt; 
+        double eta; 
+        double phi;
+       }; 
+
+      genPhotonInfo_t fSignalPhoton1;
+      genPhotonInfo_t fSignalPhoton2; 
+    
+     struct genDiPhotonInfo_t{
+        //kinematics 
+        double Minv; 
+        double qt; 
+      }; 
+    
+      genDiPhotonInfo_t fSignalDiPhoton;    
+      int numPhotons = 0;
 };
 
 //
@@ -75,7 +121,13 @@ DiPhotonAnalyzer::DiPhotonAnalyzer(const edm::ParameterSet& iConfig)
 {
    //now do what ever initialization is needed
    usesResource("TFileService");
-
+   fgenTree = fs->make<TTree>("fgenTree","GENDiphotonTree");
+    
+   fgenTree->Branch("genPhoton1", &fSignalPhoton1, "pt/D:eta:phi");
+   fgenTree->Branch("genPhoton2", &fSignalPhoton2, "pt/D:eta:phi");
+   fgenTree->Branch("genDiPhoton", &fSignalDiPhoton, "Minv/D:qt");
+   
+   genParticlesToken_ = consumes<vector<reco::GenParticle>>(iConfig.getParameter<edm::InputTag>("particles"));
 }
 
 
@@ -98,7 +150,43 @@ DiPhotonAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 {
    using namespace edm;
    using namespace std;
+   
+   //Init available in ExoDiphotons but do manual initialization here
+  fEventInfo.run = -99999.99;
+  fEventInfo.LS = -99999.99;
+  fEventInfo.evnum = -99999.99;
+
+  //An example of accessing GenParticles from the event. reco::GenParticleCollection is typedef for vector<reco::GenParticle>
+  edm::Handle<vector<reco::GenParticle> > genParticles;
+  iEvent.getByToken(genParticlesToken_,genParticles);
   
+  if(!genParticles.isValid()) {
+         cout << "No Gen Particles collection!" << endl;
+         return;
+  }
+
+  fSignalPhoton1.pt = -99999.99;
+  fSignalPhoton1.eta = -99999.99;
+  fSignalPhoton1.phi = -99999.99;
+
+  fSignalPhoton2.pt = -99999.99;
+  fSignalPhoton2.eta = -99999.99;
+  fSignalPhoton2.phi = -99999.99;
+
+  fSignalDiPhoton.Minv = -99999.99;
+  fSignalDiPhoton.qt = -99999.99;
+
+
+  for(vector<reco::GenParticle>::const_iterator ip = genParticles->begin(); ip != genParticles->end(); ++ip){
+      if(ip->status()==1 && ip->pdgId()==22){
+         cout << "Photon end state found" << endl;
+      }//end photon end state condition
+  }//end loop over gen particles 
+
+   //Fill the tree Branches 
+   fgenTree->Fill(); 
+
+
 #ifdef THIS_IS_AN_EVENT_EXAMPLE
    Handle<ExampleData> pIn;
    iEvent.getByLabel("example",pIn);
