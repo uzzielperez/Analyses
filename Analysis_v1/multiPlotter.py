400325c18506a87dcd18d7ab09a6905598ec18db
Uzziel Perez <uzzie.perez@cern.ch>
2018-06-03 02:25:17 -0500

    functional multi-purpose plotshelpr and multicomparison

--- Analysis_v1/multiPlotter.py
+++ Analysis_v1/multiPlotter.py
@@ -27,7 +27,9 @@ args = parser.parse_args()
 
 isMD = False
 
-study = "Pythia-Sherpa-NED-4_Ms_4000"
+#study = "Pythia-Sherpa-NED-4_Ms_4000"
+study = "Pythia"
+#study = "Pythia-LambdaT-4000-VarMD"
 #--------------------------------------------------------------------------------
 path = '/uscms_data/d3/cuperez/CMSSW_8_0_25/src/scripts/Analysis_v1/ADDGravToGGPythiaStudy'
 
@@ -41,7 +43,8 @@ if isMD:
 	fileDesc2 = 'LambdaT-4000_MD-1410'
 	fileDesc3 = 'LambdaT-4000_MD-1974'
 	fileDesc4 = 'LambdaT-4000_MD-2820'
-
+	
+	outName = 'MD'
 else:
 	File1 = 'TestADDG2gg_LambdaT-10000_M-500-pythia8.root'
 	File2 = 'TestADDG2gg_LambdaT-4000_M-500-pythia8.root'
@@ -52,9 +55,12 @@ else:
 	fileDesc2 = 'LambdaT-4000'
 	fileDesc3 = 'LambdaT-5000'
 	fileDesc4 = 'LambdaT-7000'
-
+	
+	outName   = 'noMD'
 FileList = [File1, File2, File3, File4]
 #FileList = args.inputfiles
+
+print FileList
 #--------------------------------------------------------------------------------
 openFileList = []
 ID = []
@@ -71,7 +77,8 @@ for fi in FileList:
 	else:
 		pattern = r'LambdaT-([^(]*)\_M-500'
 	match = re.findall(pattern, fi)
-	ID.append(match[0])
+	print fi, match
+	#ID.append(match[0])
 	h_i.append([])
 	obj_i.append([])
 	canv_i.append([])
@@ -94,165 +101,17 @@ gStyle.SetOptStat(0)
 # Arrays for file objects
 ROOT.gROOT.SetBatch(True)
 
-#Get obj and Keys
-
+# Get obj and Keys
+# Create Histograms for each
 i = 0
 for fi in openFileList:
 	LoopObjKeys(fi, obj_i[i], canv_i[i], h_i[i], i)
 	i = i + 1
 
-for objFi in obj_i:
-	j = 0
-	while j < len(objFi):
-		ytitle = 'Events'
-		c = ROOT.TCanvas()
-		scale = 1.00
-		#--------------- String Finder
-	  	if objFi[j].find("Minv") != -1:
-			xtitle = r"m_{#gamma#gamma}#scale[0.8]{(GeV)}" # r"#scale[0.8]{m_{#gamma#gamma}(GeV)}"
-	       		xmin = 500
-	       		xmax = 4000
-			#scale = 35.9
-			c.SetLogy()
-			xpos1, ypos1, xpos2, ypos2 = .40, 0.75, 1.0, .85
+#print canv_i
+#obj[0], canv_i[0], should be identical to others
+LoopOverHistogramsPerFile(study, obj_i[0], h_i, FileList, canv_i[0], outName, isMD)
 
-			#xpos1, ypos1, xpos2, ypos2 = .72, 0.75, 1.0, .85
-			#xpos1, ypos1, xpos2, ypos2 = .60, 0.65, 1.0, .85
-		elif objFi[j].find("Pt") != -1:
-		#elif objFi[j].find("Pt") != -1:
-			xtitle = "#scale[0.8]{p_{T}(GeV)}"
-			xmin = 75
-			xmax = 4000
-			#scale = 35.9
-			c.SetLogy()
-			xpos1, ypos1, xpos2, ypos2 = .40, 0.75, 1.0, .85
-			#xpos1, ypos1, xpos2, ypos2 = .60, 0.65, 1.0, .85
-
-                elif objFi[j].find("Eta") != -1:
-			xtitle = r"#eta"
-			if objFi[j].find("sc") != -1:
-			    xtitle = r"#scale[0.7]{sc} " + xtitle
-			if objFi[j].find("det") != -1:
-			    xtitle = r"#scale[0.7]{det} " + xtitle
-			xmin = -3.0
-			xmax = 3.0
-			#scale = 35.9
-			xpos1, ypos1, xpos2, ypos2 = .40, 0.75, 1.0, .85
-			#xpos1, ypos1, xpos2, ypos2 = .60, 0.65, 1.0, .85
-			#xpos1, ypos1, xpos2, ypos2 = .45, 0.20, .85, .38
-
-       	        elif objFi[j].find("Phi") != -1:
-			xtitle = r"#phi"
-			if objFi[j].find("sc") != -1:
-			    xtitle = r"#scale[0.7]{sc} " + xtitle
-			if objFi[j].find("det") != -1:
-			    xtitle = r"#scale[0.7]{det} " + xtitle
-			xmin = -3.5
-			xmax = 3.5
-			#scale = 35.9
-			xpos1, ypos1, xpos2, ypos2 = .40, 0.75, 1.0, .85
-			#xpos1, ypos1, xpos2, ypos2 = .60, 0.65, 1.0, .85
-			#xpos1, ypos1, xpos2, ypos2 = .45, 0.20, .85, .38
-
-	        else:
-	        	continue
-
-		if objFi[j].find("diphoton") != -1:
-			legentry = r"SM #gamma#gamma"
-		elif (objFi[j].find("photon1")) != -1:
-			legentry = r"#gamma_{1}"
-		elif (objFi[j].find("photon2")) != -1:
-			legentry = r"#gamma_{2}"
-		else:
-			legentry = objFi[j]
-
-	   	# EBEE or EBEB
-	   	if objFi[j].find("EBEE") != -1:
-	        	xtitle = xtitle + r" #scale[0.45]{(EBEE)}"
-	    	if objFi[j].find("EBEB") != -1:
-	        	xtitle = xtitle + r" #scale[0.45]{(EBEB)}"
-	    	# Photon1 or Photon2
-	    	if objFi[j].find("photon1") != -1:
-	        	xtitle = r"#scale[0.80]{#gamma_{1}: }" + xtitle
-	    	if objFi[j].find("photon2") != -1:
-	        	xtitle = r"#scale[0.80]{#gamma_{2}: }" + xtitle
-
-
-		
-		
-		j = j + 1
-#--------------------------------------
-#
-#         # Draw histos & additional Settings
-# 	pad1.cd()
-# 	hf1[i].Scale(scale) # rescale to 35.9 fb^-1
-# 	hf1[i].SetFillColor(kBlue -3)
-# 	hf1[i].SetTitle(obj_f1[i])
-# 	#hf1[i].GetYaxis().SetTitle("weighted Events")
-# 	hf1[i].GetYaxis().SetTitleOffset(1.4)
-# 	hf1[i].GetXaxis().SetRangeUser(xmin, xmax)
-# 	hf2[i].GetXaxis().SetRangeUser(xmin, xmax)
-# 	hf2[i].GetYaxis().SetTitle("Events")
-# 	hf2[i].SetMarkerStyle(20)
-#
-# 	hf2[i].Draw(s2)  # Draw this to set max y
-# 	hf1[i].Draw(s1)
-# 	hf2[i].Draw(s2) # To draw over filled MC
-#
-# #	hf2[i].Draw("esamex0")  # Draw this to set max y
-# #	hf1[i].Draw("hist same")
-# #	hf2[i].Draw("esamex0") # To draw over filled MC
-# 	#to avoid clipping the bottom zero, redraw a small axis
-# 	#hM.GetYaxis().SetLabelSize(0.0)
-# 	#axis = TGaxis(-5, 20, -5, 220, 20, 220, 510, "")
-# 	#axis.SetLabelFont(43)
-# 	#axis.SetLabelSize(15)
-# 	#axis.Draw()
-#
-# 	# Legend
-# 	leg = TLegend(xpos1, ypos1, xpos2, ypos2)
-# 	leg.SetBorderSize(0)
-# 	leg.SetFillColor(0)
-# 	leg.SetFillStyle(0)
-# 	leg.SetTextFont(42)
-# 	leg.SetTextSize(0.035)
-# 	leg.AddEntry(hf1[i], "%s" %(fileDescription1), "f")
-# 	leg.AddEntry(hf2[i], "%s" %(fileDescription2), "l")
-# 	leg.Draw()
-#
-#  	hRatio = createRatio(hf2[i], hf1[i])
-#         # RATIO
-# 	pad2.cd()
-# 	hf1[i].GetYaxis().SetTitle("ratio %s/%s" %(fileDescription1, fileDescription2))
-# 	#hRatio.GetXaxis().SetRangeUser(xmin, xmax)
-# 	x = hRatio.GetXaxis()
-# 	x.SetRangeUser(xmin, xmax)
-# 	x.SetTitleOffset(2.5)
-# 	x.SetTitleSize(35)
-# 	x.SetTitle(xtitle)
-# 	y = hRatio.GetYaxis()
-# 	y.SetTitle("Ratio")
-# 	y.SetTitleSize(20)
-# 	y.SetTitleFont(43)
-# 	hRatio.Draw("ep")
-#
-# 	#place_legend(c[i], None, None, None, None, header="", option="LP")
-# 	CMS_lumi(c, 4, 11, False)
-#
-# 	#c.Update()
-# 	#c.Draw()
-# 	#c.Print("BKGPlots.pdf[", "minv")
-# 	#c.Print("BKGPlotsminv.pdf")
-#
-# 	#Draw All canvases
-# 	#from ROOT import gROOT
-# 	#gROOT.GetListOfCanvases().Draw()
-# 	#gROOT.GetListOfCanvases().Print("ratioplots/ratio%s/png" %(obj_f1[i]))
-# 	c.Print("%s/%sratio_%s.png" %("ratioplots",study, obj_f1[i]))
-#
-# 	# move to next object in root file
-# 	i = i + 1
-# #-------------------------------------------
 
 sw.Stop()
 print "Processing Time:"
--- Analysis_v1/plotsHelper.py
+++ Analysis_v1/plotsHelper.py
@@ -107,44 +107,8 @@ def LoopObjKeys(fileAssign, obj_i, canvas_i, hist_i, index):
 #     #print regex, WholeExpression
 
 #def diphotonAnalysisStringFinder(objEL):
-def FilesLoop(canv, obj_f1, xmin, xmax, listofFiles, xpos1, ypos1, xpos2, ypos2, SetLogy):
-  # Draw All the Histograms in the List of Files
-    FileNum = 0
-    h[FileNum][i].Scale(scale)
-    #h[FileNum][i].GetYaxisSetTitle(obj_f1[i])
-    h[FileNum][i].SetTitle(obj_f1[i])
-    h[FileNum][i].GetYaxis().SetTitle("Events")
-    h[FileNum][i].GetYaxis().SetTitleOffset(1.4)
-    h[FileNum][i].GetXaxis().SetRangeUser(xmin, xmax)
 
-    while FileNum < len(listofFiles):
-        pattern = r'LambdaT-([^(]*)\_M-500'
-  	match = re.findall(pattern, listofFiles[FileNum])
-    	h[FileNum][i].SetLineColor(FileNum+1)
-   	#hf2[i].SetMarkerStyle(20)
-   	# Legend
-   	leg = TLegend(xpos1, ypos1, xpos2, ypos2)
-    	leg.SetBorderSize(0)
-        #leg.SetFillColor(0)
-        #leg.SetFillStyle(0)
-        leg.SetTextFont(42)
-    	leg.SetTextSize(0.035)
-    	#leg.AddEntry(h[FileNum][i], "LambdaT%s" %(fileDescription1), "f")
-    	leg.SetEntrySeparation(0.3)
-    	#leg.Draw()
-  	canv[i].cd()
-	if SetLogy:
-		canv[i].SetLogy()
-        h[FileNum][i].Draw("same")  # Draw this to set max y
-    	leg.AddEntry(h[FileNum][i], "LambdaT-%s" %(match[0]), "l")
-	canv.Update()
-
-    return canv
-    FileNum = FileNum + 1
-
-
-
-def LoopOverHistogramsPerFile(study, obj_f1, h, listofFiles, canv, outName):
+def LoopOverHistogramsPerFile(study, obj_f1, h, listofFiles, canv, outName, isMD):
     i = 0
     while i<len(obj_f1):
     	ytitle = "Events"
@@ -225,47 +189,41 @@ def LoopOverHistogramsPerFile(study, obj_f1, h, listofFiles, canv, outName):
     	if obj_f1[i].find("photon2") != -1:
     		xtitle = r"#scale[0.80]{#gamma_{2}: }" + xtitle
 
-
-	canv[i].cd()	
-	if SetLogy:
-		canv[i].SetLogy()
- 
-
          # Draw All the Histograms in the List of Files
         FileNum = 0
         #h[FileNum][i].Scale(scale)
-        #h[FileNum][i].GetYaxisSetTitle(obj_f1[i])
         h[FileNum][i].SetTitle(obj_f1[i])
-        h[FileNum][i].GetYaxis().SetTitle("Events")
-        h[FileNum][i].GetYaxis().SetTitleOffset(1.4)
+        h[FileNum][i].GetYaxis().SetTitle("Weighted Events")
+        h[FileNum][i].GetYaxis().SetTitleOffset(1.1)
         h[FileNum][i].GetXaxis().SetRangeUser(xmin, xmax)
-	
-#	if countMinv == 1:
-#		leg = TLegend(xpos1, ypos1, xpos2, ypos2)
-#		leg.SetBorderSize(0)
-#		leg.SetTextFont(42)  
-#		leg.SetTextSize(0.035)
-$# #	
-        while FileNum < len(listofFiles):
-		#canv[FileNum][i] = ROOT.TCanvas()
-        	pattern = r'LambdaT-([^(]*)\_M-500'
-        	match = re.findall(pattern, listofFiles[FileNum])
-        	h[FileNum][i].SetLineColor(FileNum+1)
-        	#hf2[i].SetMarkerStyle(20)
-		
-		leg = TLegend(xpos1, ypos1, xpos2, ypos2)
-		leg.SetBorderSize(0)
-		leg.SetTextFont(42)  
-		leg.SetTextSize(0.035)
-
-        	h[FileNum][i].Draw("same")  # Draw this to set max y
-        	leg.AddEntry(h[FileNum][i], "LambdaT-%s" %(match[0]), "l")
+	leg = TLegend(xpos1, ypos1, xpos2, ypos2)
+	leg.SetBorderSize(0)
+	leg.SetFillStyle(0)
+	leg.SetTextFont(42)
+	leg.SetTextSize(0.035)	
+        
+	while FileNum < len(listofFiles):
+		canv[i].cd()
+		if SetLogy:
+			canv[i].SetLogy()		
+		h[FileNum][i].SetLineColor(FileNum+2)
+		h[FileNum][i].Draw("hist same")
+        	if isMD:
+			pattern = r'MD/(.*)'	
+			match = re.findall(pattern, listofFiles[FileNum])
+        		leg.AddEntry(h[FileNum][i], "LambdaT-%s" %(match[0]), "l")
+		else:
+			pattern = r'LambdaT-([^(]*)\_M-500'		
+			match = re.findall(pattern, listofFiles[FileNum])
+        		leg.AddEntry(h[FileNum][i], "LambdaT-%s" %(match[0]), "l")	
+		#hf2[i].SetMarkerStyle(20)
+		leg.Draw()
+		leg.SetEntrySeparation(0.3)
 		canv[i].Update()
           
 	        FileNum = FileNum + 1
-
+		print "filenum: ", FileNum	
 	#CMS_lumi(c, 4, 11, False)
-	#canv[i].cd()
 	leg.SetEntrySeparation(0.3)
 	#leg.Draw() 
    	canv[i].Print("%s/%s%sratio_%s.png" %("ratioplots",outName, study, obj_f1[i]))
