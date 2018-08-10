bd40b9546eab953f129fa5d897e95950a13287cd
Uzziel Perez <uzzie.perez@cern.ch>
2018-08-10 02:06:04 -0500

    corrected template, naming and scripts

--- Analysis_v1/Simulation/Pythia/RunThisParamSpaceList2017-18.py
+++ Analysis_v1/Simulation/Pythia/RunThisParamSpaceList2017-18.py
@@ -8,9 +8,11 @@ HLZ = True
 
 if GRW:
 	ned         = 4 
+	negInt      = 1 
         LambdaT_lst  = [4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 10000, 11000] 
 if HLZ:
 	ned 	    = 2
+	negInt      = 0 
 	conversionfactor = pow(math.pi/2, 0.25) 
         Ms = [4000, 5000, 6000, 6500, 7000, 7500, 8000, 9000, 10000, 11000, 13000]
 	LambdaT_lst = [x * conversionfactor for x in Ms] 
@@ -40,10 +42,12 @@ for value in LambdaT_lst:
 		process       = sp.Popen(command.split(), stdout=sp.PIPE)
                 output, error = process.communicate()
 
-	        dataset       = 'ADDGravToGG_NED-%d_LambdaT-%d_M-%dTo%d_TuneCUEP8M1_%dTeV-pythia8' %(ned, int(lambdaT), minMass, maxMass, COM)
-                genfragname   =  'ADDGravToGG_NED-%d_LambdaT-%d_M-%dTo%d_TuneCUEP8M1_%dTeV-pythia8_cfi.py' %(ned, int(lambdaT), minMass, maxMass, COM)
-		
-	        print 'Generated ', dataset, '\n'
+	        dataset = 'ADDGravToGG_NegInt-%d_LambdaT-%d_M-%dTo%d_TuneCUEP8M1_%dTeV-pythia8' %(negInt, int(lambdaT), minMass, maxMass, COM)
+	        #dataset = 'ADDGravToGG_NegInt-%d_NED-%d_LambdaT-%d_M-%dTo%d_TuneCUEP8M1_%dTeV-pythia8' %(negInt, ned, int(lambdaT), minMass, maxMass, COM)	
+	        genfragname = 'ADDGravToGG_NegInt-%d_LambdaT-%d_M-%dTo%d_TuneCUEP8M1_%dTeV-pythia8_cfi.py' %(negInt, int(lambdaT), maxMass, maxMass, COM)
+	        #genfragname = 'ADDGravToGG_NegInt-%d_NED-%d_LambdaT-%d_M-%dTo%d_TuneCUEP8M1_%dTeV-pythia8_cfi.py' %(negInt, ned, int(lambdaT), maxMass, maxMass, COM)
+	        
+		print 'Generated ', dataset, '\n'
 		f = open("datasetlist2017-18.txt", "a")
 		f.write('%s\n'%(dataset))
 		fi = open("generatorFragmentList2017-18.txt", "a")
