cc001e5911127a63e2bbedd2b5b6d67a52493c59
Uzziel Perez <uzzie.perez@cern.ch>
2018-08-11 20:44:58 -0500

    Pythia output xsec parser

--- Analysis_v1/Simulation/Pythia/xsecParser.py
+++ Analysis_v1/Simulation/Pythia/xsecParser.py
@@ -1,40 +1,68 @@
 import re
+import argparse
+import os
+import subprocess as sp
+import ROOT 
+from multiprocessing import Pool
 
-filename = 'outCO-2.txt'
+parser = argparse.ArgumentParser(description='Cross Section File')
+parser.add_argument('-i', '--inputfile', help='Choose inputfile.', type=str,
+			default='outCO-2.txt')
+args = parser.parse_args()
 
-# Cross-section Information Sample 
-"""
-*-------  PYTHIA Event and Cross Section Statistics  -------------------------------------------------------------*
- |                                                                                                                 |
- | Subprocess                                    Code |            Number of events       |      sigma +- delta    |
- |                                                    |       Tried   Selected   Accepted |     (estimated) (mb)   |
- |                                                    |                                   |                        |
- |-----------------------------------------------------------------------------------------------------------------|
- |                                                    |                                   |                        |
- | f fbar -> (LED G*) -> gamma gamma             5026 |        3743        775        775 |   8.458e-12  1.373e-13 |
- | g g -> (LED G*) -> gamma gamma                5027 |        2896        225        225 |   2.249e-12  7.391e-14 |
- |                                                    |                                   |                        |
- | sum                                                |        6639       1000       1000 |   1.071e-11  1.560e-13 |
- |                                                                                                                 |
- *-------  End PYTHIA Event and Cross Section Statistics ----------------------------------------------------------*
- """
+filename = args.inputfile
 
-pattern = (r'sum\s+(.*)')
-new_file = []
 
-# Make sure file gets closed after being iterated
-with open(filename, 'r') as f:
-   lines = f.readlines()
+sw = ROOT.TStopwatch()
+sw.Start()
 
-for line in lines:
-	match = re.findall(pattern, line)
-	if match:
-		match[0].split(" ")
-		matchstring = str(match[0])
-		matchstring = matchstring.split(" ")
-		#print match
-		print matchstring
-		error = matchstring[-2]
-		xsec = matchstring[-4]
+def xsecParse(filetoparse):
+	pattern = (r'sum\s+(.*)')
+	new_file = []
 
-print xsec,"+-", error	
+	# Make sure file gets closed after being iterated
+	with open(filetoparse, 'r') as f:
+   		lines = f.readlines()
+
+	for line in lines:
+		match = re.findall(pattern, line)
+		if match:
+			match[0].split(" ")
+			matchstring = str(match[0])
+			matchstring = matchstring.split(" ")
+			#print matchstring
+			error = matchstring[-2]
+			xsec = matchstring[-4]
+
+	xsec_info = xsec + " +- " +  error
+	print xsec_info
+
+	f = open("FileInfoxsec.txt", "a")
+	f.write('%s: %s\n'%(filetoparse, xsec_info))
+	xsecListFile = open("xsecInfo.txt", "a") 
+	xsecListFile.write('%s\n'%(xsec_info))
+
+
+def ParsePythiaOutputFiles(file_list):
+	for outFile in file_list:
+		if "ADDGravToGG_NegInt" in outFile and outFile.endswith(".txt"):
+			print "Parsing ", outFile
+			xsecParse(outFile)
+
+def multiproc(file_list, func):
+	chunks = [file_list[i::5] for i in range(5)]
+	pool = Pool(processes=5)
+	result = pool.map(func, chunks)
+	#return sum(result) 
+
+
+#cmsRun_GenCard(os.listdir('.'))
+#multiproc(os.listdir('.'), cmsRun_GenCard) 	
+#xsecParse(filename)
+ParsePythiaOutputFiles(os.listdir('.'))
+
+sw.Stop()
+print ("Processing Time:")
+print ("Real time: " + str(sw.RealTime() / 60.0) + " minutes")
+print ("CPU time: " + str(sw.CpuTime() /60.0) + " minutes")
+                                                         	
