88a012964f16c7a6e009b2457d7141f51687b41c
Uzziel Perez <uzzie.perez@cern.ch>
2018-08-11 19:32:11 -0500

    added multiprocessing

--- Analysis_v1/Simulation/Pythia/EventGen.py
+++ Analysis_v1/Simulation/Pythia/EventGen.py
@@ -1,24 +1,51 @@
 import os
 import subprocess as sp
+import ROOT 
+
+from multiprocessing import Pool
+
+sw = ROOT.TStopwatch()
+sw.Start()
 
 cmsDriver_Step = False
 cmsRun_Step = True
 
-if cmsDriver_Step:
-	for filename in os.listdir('Configuration/GenProduction/python/ThirteenTeV/'):	
+def cmsDriver_GenCard(file_list):
+	for filename in file_list:
 		if "ADDGravToGG_NegInt" in filename and filename.endswith(".py"):
-			cmsDriver_cmd = "cmsDriver.py Configuration/GenProduction/python/ThirteenTeV/{} -s GEN --mc --no_exec --conditions auto:mc -n 1000".format(filename) 
+			cmsDriver_cmd = "cmsDriver.py Configuration/GenProduction/python/ThirteenTeV/{} -s GEN --mc --no_exec --conditions auto:mc -n 100".format(filename) 
 			print cmsDriver_cmd
 			process = sp.Popen(cmsDriver_cmd.split(), stdout=sp.PIPE)
 			foutput, error = process.communicate()
 
-if cmsRun_Step:
-	for fn in os.listdir('.'):
+def cmsRun_GenCard(file_list):
+	for fn in file_list:
 		if "ADDGravToGG_NegInt" in fn and fn.endswith("cfi_py_GEN.py"):
-			cmsRun_cmd = "cmsRun {} > {}.txt".format(fn, fn[:-14])
-			print cmsRun_cmd
-			run = sp.Popen(cmsRun_cmd.split(), stdout=sp.PIPE)
+			cmsRun_cmd = "cmsRun {}".format(fn)
+			logfilename = "{}.txt".format(fn[:-14])
+			logfile = open(logfilename, 'w')
+			print cmsRun_cmd, logfilename
+			run = sp.Popen(cmsRun_cmd.split(), stdout=logfile)
 			foutput, error = run.communicate()
 
-        	#if os.path.isfile(fn): # for all files in current working directory  
+        		#if os.path.isfile(fn): # for all files in current working directory  
 
+
+def multiproc(file_list, func):
+	chunks = [file_list[i::5] for i in range(5)]
+	pool = Pool(processes=5)
+	result = pool.map(func, chunks)
+	#return sum(result) 
+
+
+if cmsDriver_Step:
+	#cmsDriver_GenCard(os.listdir('Configuration/GenProduction/python/ThirteenTeV/'))
+	multiproc(os.listdir('Configuration/GenProduction/python/ThirteenTeV/'), cmsDriver_GenCard) 	
+if cmsRun_Step:
+	#cmsRun_GenCard(os.listdir('.'))
+	multiproc(os.listdir('.'), cmsRun_GenCard) 	
+
+sw.Stop()
+print ("Processing Time:")
+print ("Real time: " + str(sw.RealTime() / 60.0) + " minutes")
+print ("CPU time: " + str(sw.CpuTime() /60.0) + " minutes")
