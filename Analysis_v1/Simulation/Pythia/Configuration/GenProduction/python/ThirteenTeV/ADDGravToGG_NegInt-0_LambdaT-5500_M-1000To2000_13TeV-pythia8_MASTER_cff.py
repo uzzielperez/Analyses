1c03a149dc87a0200bfe81cc93dd6eb6411d3690
Uzziel Perez <uzzie.perez@cern.ch>
2018-08-20 14:47:48 -0500

    fragments with CP5

---
+++ Analysis_v1/Simulation/Pythia/Configuration/GenProduction/python/ThirteenTeV/ADDGravToGG_NegInt-0_LambdaT-5500_M-1000To2000_13TeV-pythia8_MASTER_cff.py
@@ -0,0 +1,36 @@
+import FWCore.ParameterSet.Config as cms 
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.Pythia8aMCatNLOSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8aMCatNLOSettingsBlock,
+        processParameters = cms.vstring(
+            'ExtraDimensionsLED:LambdaT = 5500.0',
+            'ExtraDimensionsLED:n = 2',
+            'ExtraDimensionsLED:ffbar2gammagamma = on',
+            'ExtraDimensionsLED:gg2gammagamma = on',
+            'ExtraDimensionsLED:CutOffmode = 2', 
+            'ExtraDimensionsLED:NegInt= 0',
+            'PhaseSpace:pTHatMin = 70.0',
+	    'PhaseSpace:mHatMin = 2000.0',	    
+	    'PhaseSpace:mHatMax = 1000.0', 
+            ),  
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings', 
+                                    'pythia8aMCatNLOSettings',
+				    'processParameters',
+                                    )   
+        )   
+)
+
+
