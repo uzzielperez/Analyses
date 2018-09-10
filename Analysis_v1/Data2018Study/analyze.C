d6a5247c66f6c0cb399b12d73b571922174121ea
Uzziel Perez <uzzie.perez@cern.ch>
2018-09-10 17:59:45 -0500

    Make Class for Data2018

---
+++ Analysis_v1/Data2018Study/analyze.C
@@ -0,0 +1,21 @@
+#include "ClassData2018.C" 
+#include <iostream>
+#include "TStopwatch.h"
+using namespace std;
+
+int analyze(){
+        // start stopwatch
+	TStopwatch sw;
+	sw.Start();
+
+	ClassData2018 t;
+        t.Loop();
+		
+	// stop stopwatch
+	sw.Stop();
+	cout << "Real Time: " << sw.RealTime()/60.0 << " minutes" << endl;
+	cout << "CPU Time: " << sw.CpuTime()/60.0 << " minutes" << endl;
+	return 0;
+	
+}
+
