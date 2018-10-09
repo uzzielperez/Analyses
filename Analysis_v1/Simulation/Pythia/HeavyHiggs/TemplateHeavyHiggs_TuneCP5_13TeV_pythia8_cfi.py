78a9f42132e2ef9863cda2d482da08cf9fe5483b
Uzziel Perez <uzzie.perez@cern.ch>
2018-10-09 00:50:58 -0500

    width and mass point

--- Analysis_v1/Simulation/Pythia/HeavyHiggs/TemplateHeavyHiggs_TuneCP5_13TeV_pythia8_cfi.py
+++ Analysis_v1/Simulation/Pythia/HeavyHiggs/TemplateHeavyHiggs_TuneCP5_13TeV_pythia8_cfi.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.Pythia8aMCatNLOSettings_cfi import *
 
 generator = cms.EDFilter("Pythia8GeneratorFilter",
         comEnergy = cms.double(13000.0),
@@ -15,8 +16,8 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
         pythia8CUEP8M1SettingsBlock,
         processParameters = cms.vstring(
             'HiggsSM:gg2H = on',
-            '25:m0 = 750',
-            '25:mWidth = 42.0',
+            '25:m0 = ${m0}',
+            '25:mWidth = ${mWidth}',
             '25:onMode = off',
             '25:OnIfMatch = 22 22',
             '25:doForceWidth = on',
@@ -24,7 +25,8 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
             'Higgs:clipWings = 10',
             ),
         parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CUEP8M1Settings',
+				    'pythia8CP5Settings', 
+                                    'pythia8aMCatNLOSettings',
                                     'processParameters',
                                     )
         )
