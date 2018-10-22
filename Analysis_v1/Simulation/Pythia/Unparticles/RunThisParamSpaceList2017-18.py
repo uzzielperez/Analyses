0bf3db465b495895e2187595ebe30890c295b557
Uzziel Perez <uzzie.perez@cern.ch>
2018-10-21 22:46:30 -0500

    Initial Unparticles studies

---
+++ Analysis_v1/Simulation/Pythia/Unparticles/RunThisParamSpaceList2017-18.py
@@ -0,0 +1,38 @@
+import sys
+import os 
+import subprocess as sp 
+import math
+
+du = [1.06, 1.09, 2.1]
+LambdaU = [0.4, 1, 4, 15000, 100000]
+
+       
+print "Writiting generator fragments... "
+idu = 0 
+while idu < len(du):
+	for ilambdau in LambdaU:
+		#print "du: ", du[idu], "; LambdaU: ", ilambdau
+		command = 'python runCardGenUnparticles.py -d %f -l %f' %(du[idu], ilambdau)		
+		#print command
+		#os.system(command) 
+		#process = sp.Popen(command.split(), stdout=sp.PIPE)
+		#output, error = process.communicate()
+
+		du_label        = str(du[idu])
+		du_label        = du_label.replace('.', 'p')
+		
+		ilambdau_label  = str(ilambdau)
+		ilambdau_label 	= ilambdau_label.replace('.', 'p')
+
+		#RunCard_outName = 'Unparticles_du%d_LambdaU-%d_TuneCUEP8M1_13TeV_pythia8.py' %(du, LambdaU)
+		dataset = 'Unparticles_du%s_LambdaU-%s_TuneCUEP8M1_13TeV_pythia8' %(du_label, ilambdau_label)	
+		genfrag = 'Unparticles_du%s_LambdaU-%s_TuneCUEP8M1_13TeV_pythia8_cfi.py' %(du_label, ilambdau_label)	
+		
+		print genfrag 
+		f = open("datasetUnparticles.txt", "a")
+		f.write('%s\n'%(dataset))
+                fi = open("generatorFragmentListUnparticles.txt", "a")
+                fi.write('%s\n'%(genfrag))                                                    
+	idu = idu+1
+
+
---
+++ Analysis_v1/Simulation/Pythia/Unparticles/runCardGenUnparticles.py
@@ -0,0 +1,47 @@
+import ROOT 
+import time
+import subprocess
+import os
+import argparse
+from string import Template 
+import math 
+
+## Command Line Options
+parser = argparse.ArgumentParser(description='Run Card Settings')
+parser.add_argument('-t', '--template', help='Choose Run Card template.', type=str,
+			default='TemplateUnparticlesTuneCUEP8M1.py')
+parser.add_argument('-d', '--du', type=float, default=2.1,
+ 		    help='typical du options: 1.06, 1.09, 2.1')
+parser.add_argument('-l', '--LambdaU', type=float, default=15000,
+ 		    help='?')
+parser.add_argument('-c', '--com', type=int, default=13000,
+ 		    help='Center of Mass Energy. At LHC (2018) currently at 13000 TeV')
+args = parser.parse_args()
+
+du 		= args.du
+LambdaU		= args.LambdaU
+RunCardTemplate = args.template
+
+
+du_label        = str(du)
+du_label        = du_label.replace('.', 'p')
+
+lambdau_label  = str(LambdaU)
+lambdau_label  = lambdau_label.replace('.', 'p')
+
+#Unparticles_du2x1_LambdaU-1000_TuneCUEP8M1_13TeV_pythia8.py
+RunCard_outName = 'Unparticles_du%s_LambdaU-%s_TuneCUEP8M1_13TeV_pythia8.py' %(du_label, lambdau_label) 
+filein = open(RunCardTemplate)
+
+# Dictionary
+d={'LambdaU':LambdaU,
+   'du':du}
+
+os.chdir('Unparticlesfragments')
+outfile         = open(RunCard_outName, "w+")
+src    = Template(filein.read())
+sub    = src.substitute(d) 
+outfile.write(sub)
+filein.close()
+print "Generated %s" %(RunCard_outName)
+
