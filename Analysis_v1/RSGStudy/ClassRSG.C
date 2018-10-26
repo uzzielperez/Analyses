91b93b6be0a3e1970e80ea8c8c32d50f27b76c38
Uzziel Perez <uzzie.perez@cern.ch>
2018-10-26 17:13:22 -0500

    RSG checks for EXOMC

--- Analysis_v1/RSGStudy/ClassRSG.C
+++ Analysis_v1/RSGStudy/ClassRSG.C
@@ -13,6 +13,8 @@ void ClassRSG::Loop()
  	int Ntotal      = 0;
  	int nDiphMinv   = 0;
  	int netaCut     = 0;
+  int numevents   = 10000;
+
 
     //histograms
   	TH1D* gendiphotonMinv = new TH1D("gendiphotonMinv", "", 1000, 0., 10000.);// 100, 0, 10000
@@ -33,6 +35,28 @@ void ClassRSG::Loop()
   	genphoton2Phi->Sumw2();
     gendiphotoncosthetastar->Sumw2();
 
+    //TString fileout_name = "RSG_kMpl02_M_750_py_GEN.root"; double xsec = 20.89;
+    //TString fileout_name = "RSG_kMpl02_M_1000_py_GEN.root"; double xsec = 4.892;
+    //TString fileout_name = "RSG_kMpl02_M_1250_py_GEN.root"; double xsec = 1.452;
+    //TString fileout_name = "RSG_kMpl02_M_1500_py_GEN.root"; double xsec = 0.5167;
+    //TString fileout_name = "RSG_kMpl02_M_1750_py_GEN.root"; double xsec = 0.2042;
+    //TString fileout_name = "RSG_kMpl02_M_2000_py_GEN.root"; double xsec = 0.08850;
+    //TString fileout_name = "RSG_kMpl02_M_2250_py_GEN.root"; double xsec = 0.04038;
+    //TString fileout_name = "RSG_kMpl02_M_2500_py_GEN.root"; double xsec = 0.01957;
+    //TString fileout_name = "RSG_kMpl02_M_3000_py_GEN.root"; double xsec = 0.005003;
+    //TString fileout_name = "RSG_kMpl02_M_3500_py_GEN.root"; double xsec = 0.001413;
+    //TString fileout_name = "RSG_kMpl02_M_4000_py_GEN.root"; double xsec = 0.0004246;
+    //TString fileout_name = "RSG_kMpl02_M_4500_py_GEN.root"; double xsec = 0.0001356;
+    //TString fileout_name = "RSG_kMpl02_M_4750_py_GEN.root"; double xsec = 0.00007651;
+    //TString fileout_name = "RSG_kMpl02_M_5000_py_GEN.root"; double xsec = 0.0000444;
+    //TString fileout_name = "RSG_kMpl02_M_5250_py_GEN.root"; double xsec = 0.00002617;
+    //TString fileout_name = "RSG_kMpl02_M_5500_py_GEN.root"; double xsec = 0.00001577;
+    //TString fileout_name = "RSG_kMpl02_M_5750_py_GEN.root"; double xsec = 0.000009680;
+    //TString fileout_name = "RSG_kMpl02_M_6000_py_GEN.root"; double xsec = 0.000006005;
+    //TString fileout_name = "RSG_kMpl02_M_6500_py_GEN.root"; double xsec = 0.000002464;
+    //TString fileout_name = "RSG_kMpl02_M_7000_py_GEN.root"; double xsec = 0.000001082;
+    TString fileout_name = "RSG_kMpl02_M_8000_py_GEN.root"; double xsec = 0.0000002359;
+
    Long64_t nbytes = 0, nb = 0;
    for (Long64_t jentry=0; jentry<nentries;jentry++) {
       Long64_t ientry = LoadTree(jentry);
@@ -41,7 +65,8 @@ void ClassRSG::Loop()
       Ntotal++;
 
       //double weight = 20.5*1000/10000;//xsec*nEvents/10000; xsec = 2.050e-08
-      double weight = 1.000;
+      //double weight = 1.000;
+      double weight = xsec*1000/numevents;
       // if (Cut(ientry) < 0) continue;
     //  if (GenDiPhoton_Minv > 500.){ // && GenDiPhoton_Minv < 4000.){
     		  nDiphMinv++;
@@ -64,8 +89,8 @@ void ClassRSG::Loop()
    cout << "Passed etaCut            : " << netaCut   << endl;
    cout << endl;
 
-   TFile file_out("RSG750.root", "RECREATE");
-   cout << "Writing file.." << endl;
+   std::cout << "Creating root file: " << fileout_name << std::endl;
+   TFile file_out(fileout_name, "RECREATE");
    gendiphotonMinv->Write();
    genphoton1Pt->Write();
    genphoton2Pt->Write();
