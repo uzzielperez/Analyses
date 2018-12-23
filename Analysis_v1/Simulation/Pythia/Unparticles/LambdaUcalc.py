26b2c52e1034873153b1889bb67474ceb66a736f
Uzziel <uzzie.perez@cern.ch>
2018-12-22 20:16:53 -0600

    calculate LambdaU upper limits

--- Analysis_v1/Simulation/Pythia/Unparticles/LambdaUcalc.py
+++ Analysis_v1/Simulation/Pythia/Unparticles/LambdaUcalc.py
@@ -1,6 +1,12 @@
-#du, y = 1.5, 0.3
-#du, y = 1.1, 0.09
-du, y = 2.0, 1.4
+def L(du, y):
+	return (1/y)**(1/(du-1))
 
-LambdaU = (1/y)**(1/(du-1))
-print (LambdaU)
+def xsec(du, LU):
+	return (1/LU)**(2*(du-1))
+
+print "L(1.9, 1)", L(1.9, 1)
+print "xsec(1.9, 4500.0)", xsec(1.9, 4500.0)
+print "xsec(1.9, 2500)", xsec(1.9, 2500.0)
+
+print xsec(1.9, 4500.0)/xsec(1.9, 2500.0)
+print xsec(1.9, 2500.0)/xsec(1.9, 4500.0)
