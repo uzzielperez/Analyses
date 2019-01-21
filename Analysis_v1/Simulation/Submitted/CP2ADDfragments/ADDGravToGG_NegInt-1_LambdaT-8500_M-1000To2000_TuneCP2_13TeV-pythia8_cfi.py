edf11e62f4cee81ae28b6b654a6847286f6290df
Uzziel Perez <uzzie.perez@cern.ch>
2019-01-21 22:03:47 +0100

    Submitted fragments

---
+++ Analysis_v1/Simulation/Submitted/CP2ADDfragments/ADDGravToGG_NegInt-1_LambdaT-8500_M-1000To2000_TuneCP2_13TeV-pythia8_cfi.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms 
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP2Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP2SettingsBlock,
+        processParameters = cms.vstring(
+            'ExtraDimensionsLED:LambdaT = 8500.0',
+            'ExtraDimensionsLED:n = 4',
+            'ExtraDimensionsLED:ffbar2gammagamma = on',
+            'ExtraDimensionsLED:gg2gammagamma = on',
+            'ExtraDimensionsLED:CutOffmode = 2', 
+            'ExtraDimensionsLED:NegInt= 1',
+            'PhaseSpace:pTHatMin = 70.0',
+	    'PhaseSpace:mHatMin = 1000.0',	    
+	    'PhaseSpace:mHatMax = 2000.0', 
+            ),  
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP2Settings', 
+				    'processParameters',
+                                    )   
+        )   
+)
+
+
