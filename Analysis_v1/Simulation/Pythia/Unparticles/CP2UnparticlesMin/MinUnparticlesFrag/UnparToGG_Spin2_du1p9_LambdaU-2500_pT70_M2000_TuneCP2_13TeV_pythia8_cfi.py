8c83c4e41f6d24f832482ef208ebf20e5226cb48
Uzziel <uzzie.perez@cern.ch>
2019-02-13 00:53:52 -0600

    Unparticles Minimal Request

---
+++ Analysis_v1/Simulation/Pythia/Unparticles/CP2UnparticlesMin/MinUnparticlesFrag/UnparToGG_Spin2_du1p9_LambdaU-2500_pT70_M2000_TuneCP2_13TeV_pythia8_cfi.py
@@ -0,0 +1,35 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP2Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP2SettingsBlock,
+        processParameters = cms.vstring(
+            'ExtraDimensionsUnpart:ffbar2gammagamma = on',
+            'ExtraDimensionsUnpart:gg2gammagamma = on',
+            'PromptPhoton:gg2gammagamma = on',
+            #'PromptPhoton:ffbar2gammagamma = on',
+	    'ExtraDimensionsUnpart:LambdaU = 2500.0',
+            'ExtraDimensionsUnpart:lambda = 1.0',
+            'ExtraDimensionsUnpart:dU = 1.9',
+            'ExtraDimensionsUnpart:spinU = 2',
+            'PhaseSpace:pTHatMin = 70',
+            'PhaseSpace:mHatMin = 2000',
+            'PhaseSpace:mHatMax = 1',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP2Settings',
+                                    'processParameters',
+                                    )
+        )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
