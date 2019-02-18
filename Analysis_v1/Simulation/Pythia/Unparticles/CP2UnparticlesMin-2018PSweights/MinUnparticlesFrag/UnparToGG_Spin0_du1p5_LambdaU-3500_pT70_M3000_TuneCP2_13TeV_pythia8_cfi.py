d77157e00a57a9145b29301c5983281ab98d8b88
Uzziel <uzzie.perez@cern.ch>
2019-02-18 13:06:00 -0600

    For submission.

---
+++ Analysis_v1/Simulation/Pythia/Unparticles/CP2UnparticlesMin-2018PSweights/MinUnparticlesFrag/UnparToGG_Spin0_du1p5_LambdaU-3500_pT70_M3000_TuneCP2_13TeV_pythia8_cfi.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP2Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP2SettingsBlock,
+	pythia8PSweightsSettingsBlock,
+        processParameters = cms.vstring(
+            'ExtraDimensionsUnpart:ffbar2gammagamma = on',
+            'ExtraDimensionsUnpart:gg2gammagamma = on',
+            'PromptPhoton:gg2gammagamma = on',
+            'PromptPhoton:ffbar2gammagamma = on',
+	    'ExtraDimensionsUnpart:LambdaU = 3500.0',
+            'ExtraDimensionsUnpart:lambda = 1.0',
+            'ExtraDimensionsUnpart:dU = 1.5',
+            'ExtraDimensionsUnpart:spinU = 0',
+            'PhaseSpace:pTHatMin = 70',
+            'PhaseSpace:mHatMin = 3000',
+            'PhaseSpace:mHatMax = 1',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP2Settings',
+                                    'processParameters',
+				    'pythia8PSweightsSettings',
+                                    )
+        )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
--- Analysis_v1/Simulation/Pythia/Unparticles/CP2UnparticlesMin-2018PSweights/testrunParam.py
+++ Analysis_v1/Simulation/Pythia/Unparticles/CP2UnparticlesMin-2018PSweights/testrunParam.py
@@ -23,14 +23,22 @@ if spin == "0":
 			   1.9: [2000, 2500, 3500]}
 
 
-massrange = ["500-2000", "2000-1"]
+massrange_dict = {4000:  ["500-1000", "1000-2000", "2000-4000", "4000-1"],
+		  8000:  ["500-1000", "1000-2000", "2000-4000", "4000-1"],
+		  10000: ["500-1000", "1000-2000", "2000-4000", "4000-1"],
+		  2000:  ["500-1000", "1000-2000", "2000-1"],
+		  2500:  ["500-1000", "1000-2000", "2000-1"],
+		  3000:  ["500-1000", "1000-2000", "2000-3000", "3000-1"],
+		  3500:  ["500-1000", "1000-2000", "2000-3000", "3000-1"],
+	         }
 
-print "Writiting generator fragments... "
+print "Writing generator fragments... "
 idu = 0 
 while idu < len(du):
 	print du[idu], du_LambdaU_dict[du[idu]]
 	LambdaU = du_LambdaU_dict[du[idu]]
 	for ilambdau in LambdaU:
+		massrange = massrange_dict[ilambdau]
 		for mrange in massrange:
 			#print "du: ", du[idu], "; LambdaU: ", ilambdau	
 			cmd = 'python testruncardgen.py -s %s -d %f -l %f -r %s' %(spin, du[idu], ilambdau, mrange)
