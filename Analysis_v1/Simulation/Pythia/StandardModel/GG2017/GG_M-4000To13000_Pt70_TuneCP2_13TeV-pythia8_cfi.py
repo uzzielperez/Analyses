4a463172312a55832b16701ac451d850c984db00
Uzziel <uzzie.perez@cern.ch>
2019-04-02 01:17:25 -0500

    Trimmed

--- Analysis_v1/Simulation/Pythia/StandardModel/GG2017/GG_M-4000To13000_Pt70_TuneCP2_13TeV-pythia8_cfi.py
+++ Analysis_v1/Simulation/Pythia/StandardModel/GG2017/GG_M-4000To13000_Pt70_TuneCP2_13TeV-pythia8_cfi.py
@@ -1,8 +1,7 @@
-import FWCore.ParameterSet.Config as cms 
+import FWCore.ParameterSet.Config as cms
 
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunes2017.PythiaCP2Settings_cfi import *
-from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
 
 generator = cms.EDFilter("Pythia8GeneratorFilter",
     maxEventsToPrint = cms.untracked.int32(1),
@@ -13,20 +12,18 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
     PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
         pythia8CP2SettingsBlock,
-        pythia8PSweightsSettingsBlock,
         processParameters = cms.vstring(
             'PromptPhoton:gg2gammagamma = on',
             'PromptPhoton:ffbar2gammagamma = on',
             'PhaseSpace:pTHatMin = 70',
             'PhaseSpace:mHatMin = 4000',
             'PhaseSpace:mHatMax = 13000',
-            ),  
+            ),
         parameterSets = cms.vstring('pythia8CommonSettings',
                                     'pythia8CP2Settings',
                                     'processParameters',
-                                    'pythia8PSweightsSettings',
-                                    )   
-        )   
+                                    )
+        )
 )
 
 ProductionFilterSequence = cms.Sequence(generator)
--- Analysis_v1/Simulation/Pythia/StandardModel/runCardGen.py
+++ Analysis_v1/Simulation/Pythia/StandardModel/runCardGen.py
@@ -13,29 +13,38 @@ parser.add_argument('-min', '--massminimum', default=500,
  		    help='minimum in mass bin')
 parser.add_argument('-max', '--massmaximum', default=1000,
  		    help='maximum in mass bin')
-parser.add_argument('-p', '--ptcut', type=float, default=50,
+parser.add_argument('-p', '--ptcut', type=float, default=70,
  		    help='ptcut')
 parser.add_argument('-c', '--com', type=int, default=13000,
  		    help='Center of Mass Energy. At LHC (2018) currently at 13000 TeV')
+parser.add_argument('--fall18', action="store_true")
+parser.add_argument('--fall17', action="store_true")
+
 args = parser.parse_args()
 
 massMin         = args.massminimum
 massMax         = args.massmaximum
 pTcut           = args.ptcut
 COM             = args.com/1000 # in TeV
-RunCardTemplate = args.template 
+#RunCardTemplate = args.template 
 
 RunCard_outName = 'GG_M-%sTo%s_Pt%s_TuneCP2_13TeV-pythia8_cfi.py' %(massMin, massMax, pTcut)
-
-filein = open(RunCardTemplate)
 
 # Dictionary
 d={'massMin':massMin, 
    'massMax':massMax,
    'pTcut':pTcut}
 
-outputdir = 'GG2018'
-if not os.path.exists:
+if args.fall18:
+	outputdir = 'GG2018'
+	RunCardTemplate = 'PSWeightsTemplate.py'
+if args.fall17:
+	outputdir = 'GG2017'
+	RunCardTemplate = 'TuneCP2Template.py'	
+
+filein = open(RunCardTemplate)
+
+if not os.path.exists(outputdir):
         os.mkdir(outputdir)	
 os.chdir(outputdir)
 
