ac09d72ef983903e092a72c9b879c269ed8d7e65
Uzziel <uzzie.perez@cern.ch>
2019-04-03 19:10:12 -0500

    Latest

---
+++ Analysis_v1/GenSignalDiphotonStudy/NonResonanceTemplates/analyze_ADDGravToGG_NegInt_0_LambdaT_6500_M_1000To2000.C
@@ -0,0 +1,20 @@
+#include "Class_ADDGravToGG_NegInt_0_LambdaT_6500_M_1000To2000.C"
+#include <iostream>
+#include "TStopwatch.h"
+using namespace std;
+
+int analyze_ADDGravToGG_NegInt_0_LambdaT_6500_M_1000To2000(){
+        // start stopwatch
+	TStopwatch sw;
+	sw.Start();
+
+	Class_ADDGravToGG_NegInt_0_LambdaT_6500_M_1000To2000 t;
+        t.Loop();
+
+	// stop stopwatch
+	sw.Stop();
+	cout << "Real Time: " << sw.RealTime()/60.0 << " minutes" << endl;
+	cout << "CPU Time: " << sw.CpuTime()/60.0 << " minutes" << endl;
+	return 0;
+
+}
---
+++ Analysis_v1/GenSignalDiphotonStudy/eoshelpers/README.md
@@ -0,0 +1,7 @@
+# EOSHELPER 
+1.) Run eoshelper to look for the full path of the files you need. 
+```bash
+
+./eoshelper.sh
+
+```
