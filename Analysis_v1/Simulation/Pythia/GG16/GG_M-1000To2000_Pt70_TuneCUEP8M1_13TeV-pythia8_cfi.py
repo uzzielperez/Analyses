8d08b105f04dc3f5c162e1085678877b937f048d
Uzziel Perez <uzzie.perez@cern.ch>
2019-10-29 03:11:50 +0100

    GG tunecuep8m1

---
+++ Analysis_v1/Simulation/Pythia/GG16/GG_M-1000To2000_Pt70_TuneCUEP8M1_13TeV-pythia8_cfi.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP2SettingsBlock,
+        processParameters = cms.vstring(
+            'PromptPhoton:gg2gammagamma = on',
+            'PromptPhoton:ffbar2gammagamma = on',
+            'PhaseSpace:pTHatMin = 70',
+            'PhaseSpace:mHatMin = 1000',
+            'PhaseSpace:mHatMax = 2000',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
