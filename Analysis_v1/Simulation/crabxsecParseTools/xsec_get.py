89fb34018803475dbd413c2d3200b768d35303fa
Uzziel <uzzie.perez@cern.ch>
2020-05-12 18:47:45 -0500

    update crabXsec Instructions and more robust for existing filenames or dir

--- Analysis_v1/Simulation/crabxsecParseTools/xsec_get.py
+++ Analysis_v1/Simulation/crabxsecParseTools/xsec_get.py
@@ -1,31 +1,40 @@
 import os
 import sys
-import re 
+import re
 
 #f = open("copythese.txt")
 f = open("parsedis.txt")
 lines = f.read().split('\n') #list containing each line
 lines = lines[:-1] #to exclude last slots in lines which is white space
 
-for line in lines:	
+for line in lines:
 	print "NEW: ",line
-	pattern = r'DiPhotonAnalysis/xsec/([^(]*)_pythia8/crab' 
-	match = re.findall(pattern, line)	
+	pattern = r'Triphoton/xsec/([^(]*)_pythia8/crab'
+	# pattern = r'DiPhotonAnalysis/xsec/([^(]*)_pythia8/crab'
+	match = re.findall(pattern, line)
 	#print match
 	dataset = match[0]
-	tarredsets = ".".join((dataset, "tar.gz")) 
-	
-	# Copy the output 
+	if os.path.exists(dataset):
+		dataset = dataset + "2"
+	tarredsets = ".".join((dataset, "tar.gz"))
+
+	# Copy the output
 	cmd = 'xrdcp %s %s' %(line, tarredsets)
 	print cmd
-	os.system(cmd)	
-	mkdircmd = 'mkdir %s' %(dataset)
+	os.system(cmd)
+	dataset_dir = dataset
+	if (os.path.isdir(dataset_dir)):
+		# if the directory name already exists
+		# append 2
+		dataset_dir = dataset + "2"
+	else:
+		dataset_dir = dataset
+	mkdircmd = 'mkdir %s' %(dataset_dir)
 	os.system(mkdircmd)
-	
-	# Untar 
-	tarcmd = 'tar -xvf %s -C %s' %(tarredsets, dataset)
-	os.system(tarcmd)  
+
+	# Untar
+	tarcmd = 'tar -xvf %s -C %s' %(tarredsets, dataset_dir)
+	os.system(tarcmd)
 	print tarcmd
 	#print "copying %s" %(fileout)
 	#os.system(cmd)
-
